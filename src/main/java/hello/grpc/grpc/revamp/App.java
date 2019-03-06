56074bb9ee0fb2dca60f23f48727bea26b40fa28
isaachambers <isaaceduardk@gmail.com>
2019-03-06 16:18:18 +0300

    First Commit

---
+++ src/main/java/hello/grpc/grpc/revamp/App.java
@@ -0,0 +1,13 @@
+package hello.grpc.grpc.revamp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("tea1m".hashCode() == "meat1".hashCode());
+	}
+
+}
